f518e15544ae1acd47093085adaf09e344d10298
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-06-04 00:35:58 +0900

    PG_level2_숫자카드나누기 Gcd 풀이

--- programmers/level2/PG_숫자_카드_나누기.java
+++ programmers/level2/PG_숫자_카드_나누기.java
@@ -8,42 +8,52 @@ public class PG_숫자_카드_나누기 {
 
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        Set<Integer> setA = Arrays.stream(arrayA).boxed().collect(Collectors.toSet());
-        Set<Integer> setB = Arrays.stream(arrayB).boxed().collect(Collectors.toSet());
 
-        int maxArrayA = arrayA[0];
-        int maxArrayB = arrayB[0];
+        int minA = getMin(arrayA);
+        int minB = getMin(arrayB);
 
-        int max = Math.max(maxArrayA, maxArrayB);
+        int gcdA = getGcd(arrayA, minA);
+        int gcdB = getGcd(arrayB, minB);
 
-        for (int i = 1; i <= 100000000; i++) {
-            if (i > max) break;
-            if (shouldOdd(setA, i) && shouldNotOdd(setB, i)) answer = i;
-            if (shouldOdd(setB, i) && shouldNotOdd(setA, i)) answer = i;
-        }
+        answer = Math.max(getAnswer(gcdA, arrayB), answer);
+        answer = Math.max(getAnswer(gcdB, arrayA), answer);
 
         return answer;
     }
 
-    public boolean shouldOdd(Set<Integer> target, int number) {
-        // boolean result = false;
-
+    private int getAnswer(int gcd, int[] target) {
         for (int a : target) {
-            if (a % number != 0) {
-                return false;
+            if (a % gcd == 0) {
+                return 0;
             }
         }
-        return true;
+        return gcd;
     }
 
-    public boolean shouldNotOdd(Set<Integer> target, int number) {
+    private int getMin(int[] target) {
+        return Arrays.stream(target)
+                .reduce((a, b) -> Math.min(a, b))
+                .getAsInt();
+    }
+
+    private int getGcd(int[] target, int minValue) {
+        int gcd = 1;
+
+        for (int i = 2; i <= minValue; i++) {
+            if (isGcd(target, i)) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    private boolean isGcd(int[] target, int value) {
         for (int a : target) {
-            if (a % number == 0) {
+            if (a % value != 0) {
                 return false;
             }
         }
         return true;
-
     }
 }
 
